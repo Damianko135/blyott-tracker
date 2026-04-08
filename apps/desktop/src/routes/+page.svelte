e0d2195a5e80ac38e5947389b48e56be968a194c
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2026-04-08 13:09:30 +0200

    Initial commit

---
+++ apps/desktop/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { resolve } from '$app/paths';
+  import { Badge } from '@svelteplex/svelte/shadcn/badge';
+</script>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+  <div class="-mt-20 flex flex-col items-center">
+    <Badge
+      class="border-neutral-300 bg-white/75 px-4 py-1 text-sm text-neutral-600"
+      variant="outline"
+    >
+      Mono
+    </Badge>
+    <h1 class="font-garamond -mt-2 text-9xl font-medium">SveltePlex</h1>
+    <h2 class="font-garamond mt-5 text-lg">Monorepo for Everything Svelte</h2>
+    <a href={resolve('/second')} class="mt-4 text-neutral-500 underline hover:text-neutral-800">
+      Go to the second route
+    </a>
+  </div>
+</div>
---
+++ apps/desktop/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: '.vite/renderer/main_window'
+    }),
+    alias: {
+      '@lib': '../../packages/svelte/src/lib',
+      '@lib/*': '../../packages/svelte/src/lib/*'
+    }
+  }
+};
+
+export default config;
